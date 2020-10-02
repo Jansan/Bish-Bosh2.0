45d7864796cb5404a0f846c4665c0c7bae852e66
jansan <jansan-26@hotmail.com>
2020-10-02 17:00:04 +0200

    get bish value

--- js/bishbosh.js
+++ js/bishbosh.js
@@ -1,23 +1,25 @@
-
+document.querySelector('#start').addEventListener('click', BishBosh);
 function BishBosh() {
 
-    let myCollection = document.getElementById('#bish-bosh');
+    let myCollection = document.getElementById('#bish').value;
+
+    let demo = document.getElementsByTagName("p");
+
     for (let i = 1; i < 100; i++) {
         if (i % 3 == 0 && i % 4 == 0) {
             for (let i = 0; 0 < myCollection.length; i++) {
                 myCollection[i].innerHTML = "Bish-Bosh";
                 console.log(myCollection);
             }
-            // document.querySelector('#bish-bosh').textContent = "Bish-Bose";
 
         } else if (i % 3 == 0) {
-            // document.querySelector('#bish').textContent = "Bish";
+
             console.log("Bish");
         } else if (i % 4 == 0) {
-            // document.querySelector('#bosh').textContent = i + " Bosh";
+
             console.log("Bosh");
         } else {
-            //  document.querySelector('#i').textContent = i;
+
             console.log(i);
         }
     }
